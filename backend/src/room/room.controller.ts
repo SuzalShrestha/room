30e6a4f9340b91cb49b9c519d6b8d3043b52b495
sujalshrestha <sujalshresthawork@gmail.com>
2024-05-28 15:35:12 +0545

    signed commit

--- backend/src/room/room.controller.ts
+++ backend/src/room/room.controller.ts
@@ -1,27 +1,51 @@
-import { Body, Controller, Get, Param, Post, Put, Query, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { RoomService } from './room.service';
 import { Room } from './schemas/room.schema';
 import { CreateRoomDto } from './dto/create-room.dto';
 import { UpdateRoomDto } from './dto/update-room.dto';
 import { Query as ExpressQuery } from 'express-serve-static-core';
-import { ApiCreatedResponse, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiCreatedResponse,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { JwtAuthGuard } from 'src/auth/guards/jwt.guard';
 
-@ApiTags("Rooms")
+@ApiTags('Rooms')
 @Controller('rooms')
 export class RoomController {
   constructor(private roomService: RoomService) {}
 
   @Get()
-  @ApiResponse({status: 200 ,description: "All rooms fetched.", type: Room, isArray: true})
-  @ApiQuery({ name: 'page', required: false, type: Number, description: 'Page number' })
+  @ApiResponse({
+    status: 200,
+    description: 'All rooms fetched.',
+    type: Room,
+    isArray: true,
+  })
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    type: Number,
+    description: 'Page number',
+  })
   @UseGuards(JwtAuthGuard)
   async getAllRooms(@Query() query: ExpressQuery): Promise<Room[]> {
     return this.roomService.getAllRooms(query);
   }
 
   @Get(':slug')
-  @ApiResponse({ description: 'Room fetched.', type: Room , status: 200})
+  @ApiResponse({ description: 'Room fetched.', type: Room, status: 200 })
   async getRoomBySlug(
     @Param('slug')
     slug: string,
@@ -38,9 +62,8 @@ export class RoomController {
     return this.roomService.createRoom(room);
   }
 
-
   @Put(':id')
-  @ApiResponse({description: "Room Updated.", type: Room, status: 200})
+  @ApiResponse({ description: 'Room Updated.', type: Room, status: 200 })
   async updateRoom(
     @Param('id')
     id: string,
